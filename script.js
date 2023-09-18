5d16e7e5b8fa2b37e764ab146383a6b83f3869eb
me <me@gmail.com>
2023-09-18 09:23:44 +0200

    third commit

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('javascript is connected')
+//DOM
+const music = document.querySelector('audio');
+const pevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+
+//Check if Audio is playing
+let isPlaying = false;
+
+//Play Audio
+function playSong() {
+    isPlaying = true;
+    playBtn.classList.replace('fa-play', 'fa-pause')
+    playBtn.setAttribute('title', 'Pause');
+    music.play();
+}
+
+//Pause Audio
+function pauseSong() {
+    isPlaying = false;
+    playBtn.classList.replace('fa-pause', 'fa-play')
+    playBtn.setAttribute('title', 'Play');
+    music.pause();
+}
+
+//Event Listener: Play or Pause
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
